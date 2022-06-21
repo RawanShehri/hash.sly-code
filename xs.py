283139320ba627c6379e09c049dafa03696ecc61
sly code <80153976+RawanShehri@users.noreply.github.com>
2022-06-21 14:48:31 +0300

    Update xs.py

--- xs.py
+++ xs.py
@@ -4,6 +4,10 @@ import hashlib
 
 o=input("your Text -> ")
 
-g=hashlib.sha1(b"{}".format(o).hexdigest()
+g=hashlib.sha1(b"{}".format(o)).hexdigest()
+
+ft=len(g)
+
+print(f"number of symbols -> {ft}")
 
 print(f"your hash -> {g}")
